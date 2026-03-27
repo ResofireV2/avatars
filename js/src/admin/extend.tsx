ea9961fe56a152e5312b62bcaab21f33cee70497
ResofireV2 <stryguardian@gmail.com>
2026-03-27 16:37:54 -0500

    Add files via upload

--- js/src/admin/extend.tsx
+++ js/src/admin/extend.tsx
@@ -2,61 +2,62 @@ import Extend from 'flarum/common/extenders';
 import app from 'flarum/admin/app';
 
 let flushing = false;
-let flushMessage = '';
-
-function FlushSetting() {
-  const flush = () => {
-    if (flushing) return;
-    flushing = true;
-    flushMessage = '';
-    app.request({ method: 'POST', url: app.forum.attribute('apiUrl') + '/resofire-avatars/flush' })
-      .then((data: any) => {
-        flushMessage = app.translator.trans('resofire-avatars.admin.flush_success', {
-          users: data.flushed, files: data.filesDeleted,
-        }) as string;
-      })
-      .catch(() => {
-        flushMessage = app.translator.trans('resofire-avatars.admin.flush_error') as string;
-      })
-      .finally(() => { flushing = false; m.redraw(); });
-  };
-
-  return (
-    <div className="Form-group">
-      <label>{app.translator.trans('resofire-avatars.admin.flush_label')}</label>
-      <p className="helpText">{app.translator.trans('resofire-avatars.admin.flush_help')}</p>
-      <button className="Button Button--danger" onclick={flush} disabled={flushing}>
-        {flushing
-          ? app.translator.trans('resofire-avatars.admin.flush_running')
-          : app.translator.trans('resofire-avatars.admin.flush_button')}
-      </button>
-      {flushMessage ? <p style={{ marginTop: '8px' }}>{flushMessage}</p> : null}
-    </div>
-  );
-}
+let flushMessage: any = '';
 
 export default [
   new Extend.Admin()
     .setting(() => ({
       setting: 'resofire-avatars.default_style',
-      label: app.translator.trans('resofire-avatars.admin.default_style') as string,
-      help: app.translator.trans('resofire-avatars.admin.default_style_help') as string,
+      label: app.translator.trans('resofire-avatars.admin.default_style'),
+      help: app.translator.trans('resofire-avatars.admin.default_style_help'),
       type: 'select',
       options: {
-        random:        app.translator.trans('resofire-avatars.admin.style_options.random') as string,
-        cyberpunk:     app.translator.trans('resofire-avatars.admin.style_options.cyberpunk') as string,
-        android:       app.translator.trans('resofire-avatars.admin.style_options.android') as string,
-        orc:           app.translator.trans('resofire-avatars.admin.style_options.orc') as string,
-        undead:        app.translator.trans('resofire-avatars.admin.style_options.undead') as string,
-        pirate:        app.translator.trans('resofire-avatars.admin.style_options.pirate') as string,
-        glitch:        app.translator.trans('resofire-avatars.admin.style_options.glitch') as string,
-        emoji:         app.translator.trans('resofire-avatars.admin.style_options.emoji') as string,
-        'sugar-skull': app.translator.trans('resofire-avatars.admin.style_options.sugar-skull') as string,
-        'lcd-face':    app.translator.trans('resofire-avatars.admin.style_options.lcd-face') as string,
-        cassette:      app.translator.trans('resofire-avatars.admin.style_options.cassette') as string,
-        eye:           app.translator.trans('resofire-avatars.admin.style_options.eye') as string,
-        treant:        app.translator.trans('resofire-avatars.admin.style_options.treant') as string,
+        random:        app.translator.trans('resofire-avatars.admin.style_options.random'),
+        cyberpunk:     app.translator.trans('resofire-avatars.admin.style_options.cyberpunk'),
+        android:       app.translator.trans('resofire-avatars.admin.style_options.android'),
+        orc:           app.translator.trans('resofire-avatars.admin.style_options.orc'),
+        undead:        app.translator.trans('resofire-avatars.admin.style_options.undead'),
+        pirate:        app.translator.trans('resofire-avatars.admin.style_options.pirate'),
+        glitch:        app.translator.trans('resofire-avatars.admin.style_options.glitch'),
+        emoji:         app.translator.trans('resofire-avatars.admin.style_options.emoji'),
+        'sugar-skull': app.translator.trans('resofire-avatars.admin.style_options.sugar-skull'),
+        'lcd-face':    app.translator.trans('resofire-avatars.admin.style_options.lcd-face'),
+        cassette:      app.translator.trans('resofire-avatars.admin.style_options.cassette'),
+        eye:           app.translator.trans('resofire-avatars.admin.style_options.eye'),
+        treant:        app.translator.trans('resofire-avatars.admin.style_options.treant'),
       },
     }))
-    .customSetting(() => <FlushSetting />),
+    .customSetting(() => {
+      const flush = () => {
+        if (flushing) return;
+        flushing = true;
+        flushMessage = '';
+        app.request({ method: 'POST', url: app.forum.attribute('apiUrl') + '/resofire-avatars/flush' })
+          .then((data: any) => {
+            flushing = false;
+            flushMessage = app.translator.trans('resofire-avatars.admin.flush_success', {
+              users: data.flushed, files: data.filesDeleted,
+            });
+            m.redraw();
+          })
+          .catch(() => {
+            flushing = false;
+            flushMessage = app.translator.trans('resofire-avatars.admin.flush_error');
+            m.redraw();
+          });
+      };
+
+      return (
+        <div className="Form-group">
+          <label>{app.translator.trans('resofire-avatars.admin.flush_label')}</label>
+          <p className="helpText">{app.translator.trans('resofire-avatars.admin.flush_help')}</p>
+          <button className="Button Button--danger" onclick={flush} disabled={flushing}>
+            {flushing
+              ? app.translator.trans('resofire-avatars.admin.flush_running')
+              : app.translator.trans('resofire-avatars.admin.flush_button')}
+          </button>
+          {flushMessage ? <p style={{ marginTop: '8px' }}>{flushMessage}</p> : null}
+        </div>
+      );
+    }),
 ];
